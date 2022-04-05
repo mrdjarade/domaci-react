df17c547779a2d052e42197b033275bb113ba0b7
mrdjarade <radem98@gmail.com>
2022-04-06 00:21:20 +0200

    Ispravljen prikaz brojeva

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -4,16 +4,20 @@ const Balance = (props) => {
   return (
     <div className={classes["balance-container"]}>
       <h2 className={classes.heading}>
-        Ukupno: {props.data.income + props.data.expenses} RSD
+        Ukupno: {(props.data.income + props.data.expenses).toLocaleString()} RSD
       </h2>
       <div className={classes["balance-container--cards"]}>
         <div>
           <h2 className={classes.heading}>Prihodi:</h2>
-          <p className={classes.income}>{props.data.income} RSD</p>
+          <p className={classes.income}>
+            {props.data.income.toLocaleString()} RSD
+          </p>
         </div>
         <div>
           <h2 className={classes.heading}>Rashodi:</h2>
-          <p className={classes.expense}>{props.data.expenses} RSD</p>
+          <p className={classes.expense}>
+            {props.data.expenses.toLocaleString()} RSD
+          </p>
         </div>
       </div>
     </div>
